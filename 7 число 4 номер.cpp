dc082dec9a503085c10ddcc33fe0e486b08b5ba9
theblackone79 <150225131+theblackone79@users.noreply.github.com>
2023-11-10 11:46:47 +0300

    Add files via upload

---
+++ 7 число 4 номер.cpp
@@ -0,0 +1,43 @@
+﻿
+
+#include <iostream>
+#include <string>
+using namespace std;
+bool ValidatePassword(const string& password)
+{
+
+    if (password.length() < 8) {
+        return false;
+    }
+
+    for (char c : password)
+    {
+        if (!((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') ||
+            (c == ':' || c == '!' || c == '@' || c == '#' || c == '$' ||
+                c == '%' || c == '&' || c == '*' || c == '+' || c == '?'))) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+
+
+int main() 
+{
+    using namespace std;
+    setlocale(0, "");
+    string password;
+    cout << "Введите пароль: ";
+    cin >> password;
+
+    if (ValidatePassword(password)) {
+        cout << "Пароль допустим." << endl;
+    }
+    else {
+        cout << "Пароль недопустим." << endl;
+    }
+
+    return 0;
+}
---
+++ 7 число номер 3.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(0, "");
+    int nums[14] = {
+        0,1,2,3,4,5,6,7,8,9,12,19,17,34
+    };
+    int sum = 0;
+    int num = 0;
+    for (int i = 0; i < 14; i++)
+    {
+        num = nums[i];
+        if (!(num % 2 == 0))
+        {
+            sum += num;
+        }
+    }
+    cout << "сумма всех нечётных чисел: " << sum << endl;
+    return 0;
+}
